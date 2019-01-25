356075b7d3433292571247168908824493e36bdb
ruby <916809498@qq.com>
2019-01-25 15:27:46 +0800

    logo

--- deploy.php
+++ deploy.php
@@ -23,10 +23,13 @@ $files = [
     'popup.css',
     'popup.html',
     'popup.js',
+    'logos/16x16.png',
+    'logos/48x48.png',
+    'logos/128x128.png',
 ];
 
 foreach ($files as $file) {
-    if (!$zip->addFile(__DIR__ . DIRECTORY_SEPARATOR . $file)) {
+    if (!$zip->addFile(__DIR__ . DIRECTORY_SEPARATOR . $file, $file)) {
         die("add file: {$file} fails!" . PHP_EOL);
     }
 }
