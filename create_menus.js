48db6dbb33dc64c8a0695932e03426a6ddfd1e4e
ruby <916809498@qq.com>
2019-01-25 19:25:05 +0800

    admin login script

--- create_menus.js
+++ create_menus.js
@@ -9,50 +9,81 @@ let currentInfo, currnetTab
  */
 function getLoginScript(username, password) {
     let loginScript = `
-    obj = {}
-
-    document.querySelector('.header-top .left li i').addEventListener('click', evt => {
-        let user = {
-            username: '${username}',
-            password: '${password}'
-        };
-
-        let times = 0
-        let id = setInterval(() => {
-            if (times++ == 120) {
-                clearInterval(id)
-            }
-
-            let username = document.querySelector('input[placeholder="用户名/手机号码"]');
-            if (username) {
-                input(username, user.username)
-                let password = document.querySelector('input[placeholder="登录密码"]');
-                input(password, user.password)
-
-                let submit = document.querySelector('.remember button');
-                submit.click();
-            }
-        }, 1000)
-    })
-
-    // 退出按钮
-    logout = document.querySelector('.header-top .left li:last-child')
-    if (JSON.parse(localStorage.getItem('userLoginState')) === true) {
-        obj['refreshed'] = true
-        logout.click()
-    } else {
-        obj['refreshed'] = false
-        // 登录弹窗
-        document.querySelector('.header-top .left li i').click()
-    }
-
     function input(el, value) {
         el.value = value
         el.dispatchEvent(new Event('input'))
         el.dispatchEvent(new Event('change'))
         el.dispatchEvent(new Event('blur'))
     }
-
+    
+    obj = {}
+    if (location.href.indexOf('shop') === -1) {
+        dropdown = document.querySelectorAll('.user-dropdown li')
+        if (dropdown.length > 0) {
+            // 管理后台(已经登录)
+            logout = dropdown[dropdown.length - 1].children[0]
+            obj['refreshed'] = true
+            logout.click()
+        } else {
+            let user = {
+                username: '${username}',
+                password: '${password}'
+            };
+    
+            let times = 0
+            let id = setInterval(() => {
+                if (times++ == 120) {
+                    clearInterval(id)
+                }
+    
+                let username = document.querySelector('input[name="username"]')
+                if (username) {
+                    input(username, user.username)
+                    let password = document.querySelector('input[name="password"]')
+                    input(password, user.password)
+    
+                    let submit = document.querySelector('.login-submit span')
+                    submit.click();
+                }
+            }, 1000)
+        }
+    } else {
+        // 商城
+        document.querySelector('.header-top .left li i').addEventListener('click', evt => {
+            let user = {
+                username: '${username}',
+                password: '${password}'
+            };
+    
+            let times = 0
+            let id = setInterval(() => {
+                if (times++ == 120) {
+                    clearInterval(id)
+                }
+    
+                let username = document.querySelector('input[placeholder="用户名/手机号码"]');
+                if (username) {
+                    input(username, user.username)
+                    let password = document.querySelector('input[placeholder="登录密码"]');
+                    input(password, user.password)
+    
+                    let submit = document.querySelector('.remember button');
+                    submit.click();
+                }
+            }, 1000)
+        })
+    
+        // 退出按钮
+        logout = document.querySelector('.header-top .left li:last-child')
+        if (JSON.parse(localStorage.getItem('userLoginState')) === true) {
+            obj['refreshed'] = true
+            logout.click()
+        } else {
+            // 登录弹窗
+            document.querySelector('.header-top .left li i').click()
+        }
+    }
+    
     obj
     `
 
@@ -72,7 +103,7 @@ function chooseMenu(info, tab) {
         code: loginScript
     }, items => {
         // 页面刷新过
-        if (items[0].refreshed) {
+        if (items[0] && items[0].refreshed) {
             chrome.tabs.onUpdated.addListener(function (tabId, info) {
                 if (tabId === tab.id && info.status === 'complete') {
                     chrome.tabs.executeScript(tab.id, {
--- login_admin.js
+++ login_admin.js
@@ -1,43 +1,76 @@
-// list = document.querySelectorAll('.user-dropdown li')
-// logout = list[list.length - 1]
-
-document.querySelector('.header-top .left li i').addEventListener('click', evt => {
-    console.log(evt)
-    let user = {
-        username: 'blyy123',
-        password: '123456'
-    };
-
-    let times = 0
-    let id = setInterval(() => {
-        if (times++ == 120) {
-            clearInterval(id)
-        }
-
-        let username = document.querySelector('input[placeholder="用户名/手机号码"]');
-        if (username) {
-            input(username, user.username)
-            let password = document.querySelector('input[placeholder="登录密码"]');
-            input(password, user.password)
-
-            let submit = document.querySelector('.remember button');
-            submit.click();
-        }
-    }, 1000)
-})
-
-// 退出按钮
-let logout = document.querySelector('.header-top .left li:last-child')
-if (JSON.parse(localStorage.getItem('userLoginState')) === true) {
-    logout.click()
-}
-
-// 登录弹窗
-document.querySelector('.header-top .left li i').click()
-
 function input(el, value) {
     el.value = value
     el.dispatchEvent(new Event('input'))
     el.dispatchEvent(new Event('change'))
     el.dispatchEvent(new Event('blur'))
 }
+
+obj = {}
+if (location.href.indexOf('shop') === -1) {
+    dropdown = document.querySelectorAll('.user-dropdown li')
+    if (dropdown.length > 0) {
+        // 管理后台(已经登录)
+        logout = dropdown[dropdown.length - 1].children[0]
+        obj['refreshed'] = true
+        logout.click()
+    } else {
+        let user = {
+            username: '${username}',
+            password: '${password}'
+        };
+
+        let times = 0
+        let id = setInterval(() => {
+            if (times++ == 120) {
+                clearInterval(id)
+            }
+
+            let username = document.querySelector('input[name="username"]')
+            if (username) {
+                input(username, user.username)
+                let password = document.querySelector('input[name="password"]')
+                input(password, user.password)
+
+                let submit = document.querySelector('.login-submit span')
+                submit.click();
+            }
+        }, 1000)
+    }
+} else {
+    // 商城
+    document.querySelector('.header-top .left li i').addEventListener('click', evt => {
+        let user = {
+            username: '${username}',
+            password: '${password}'
+        };
+
+        let times = 0
+        let id = setInterval(() => {
+            if (times++ == 120) {
+                clearInterval(id)
+            }
+
+            let username = document.querySelector('input[placeholder="用户名/手机号码"]');
+            if (username) {
+                input(username, user.username)
+                let password = document.querySelector('input[placeholder="登录密码"]');
+                input(password, user.password)
+
+                let submit = document.querySelector('.remember button');
+                submit.click();
+            }
+        }, 1000)
+    })
+
+    // 退出按钮
+    logout = document.querySelector('.header-top .left li:last-child')
+    if (JSON.parse(localStorage.getItem('userLoginState')) === true) {
+        obj['refreshed'] = true
+        logout.click()
+    } else {
+        // 登录弹窗
+        document.querySelector('.header-top .left li i').click()
+    }
+}
+
+obj
